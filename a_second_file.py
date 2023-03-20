72552858f5ccb8cc05710cbf66b08f9042a80ecf
Pelliza Candela Sol <candela.pelliza@stud.plus.ac.at>
2023-03-20 11:39:21 +0100

    second commit

---
+++ a_second_file.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+print(sys.path)
+help("modules")
+days_of_the_week = 7def 
+say_hello(recipient):
+    print("Hello, world! Hello {} ".format(recipient))
+    return recipient
